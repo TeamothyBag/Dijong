64f3e77ae1c5969a40d668b4b04009ae74a767ef
Teamothy <st10092757@imconnect.edu.za>
2023-08-22 08:42:54 +0200

    hello

---
+++ AdminDashboard.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./AdminDashboard.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
